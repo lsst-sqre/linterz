fb91d65420e04118517e48da6c0300cb2ea68077
Joshua Hoblitt <josh@hoblitt.com>
2019-01-29 12:08:34 -0700

    add terraform.sh

---
+++ tests/terraform.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+TF_VER="0.11.11"
+
+tf() {
+  docker run -ti -v "$(pwd):$(pwd)" -w "$(pwd)" "hashicorp/terraform:${TF_VER}" "$@"
+}
+
+tf fmt --check=true --diff=true
+tf validate --check-variables=false
+
+# vim: tabstop=2 shiftwidth=2 expandtab
